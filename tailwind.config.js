5d0b203c572ddff6733bdae5731e9404be20e103
Arishah Khan <arishahsanaullah78@gmail.com>
2024-12-04 09:07:28 +0500

    working

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./main.js",
+    "./dashboard.html",
+    "./signIn.html",
+    "./signIn.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
